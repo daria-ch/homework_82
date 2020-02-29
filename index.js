3477fbd33f1050724afb37057befaa458ed7427a
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-02-29 10:58:39 +0600

    fixed albums resource

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(cors());
 app.use(express.static("public"));
 
 const run = async () => {
-    await mongoose.connect("mongodb://localhost/shop", {
+    await mongoose.connect("mongodb://localhost/music", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
 
     app.use("/artists", artists);
     app.use("/albums", albums);
-    app.use("/tracks", tracks);
+    // app.use("/tracks", tracks);
 
     app.listen(port, () => {
         console.log(`Server started on ${port} port!`);

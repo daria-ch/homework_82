de3d35a5fab2857026a42b85ab122c990dc0a470
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-03-06 13:28:32 +0600

    added User schema

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.static("public"));
 const run = async () => {
     await mongoose.connect("mongodb://localhost/music", {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true
     });
 
     app.use("/artists", artists);
